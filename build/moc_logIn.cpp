e2f49c522cd0995d195575215f08f409d8b33482
Matt Archer <matthewarcherr@gmail.com>
2022-02-20 16:38:35 +0000

    absolute -rpath

--- build/moc_logIn.cpp
+++ build/moc_logIn.cpp
@@ -1,18 +1,19 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'logIn.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.8)
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.2.3)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
+#include <memory>
 #include "../src/main/logIn.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'logIn.h' doesn't include <QObject>."
-#elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.12.8. It"
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.2.3. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -21,42 +22,45 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_logIn_t {
-    QByteArrayData data[3];
-    char stringdata0[13];
+    const uint offsetsAndSize[10];
+    char stringdata0[36];
 };
-#define QT_MOC_LITERAL(idx, ofs, len) \
-    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_logIn_t, stringdata0) + ofs \
-        - idx * sizeof(QByteArrayData)) \
-    )
+#define QT_MOC_LITERAL(ofs, len) \
+    uint(offsetof(qt_meta_stringdata_logIn_t, stringdata0) + ofs), len 
 static const qt_meta_stringdata_logIn_t qt_meta_stringdata_logIn = {
     {
-QT_MOC_LITERAL(0, 0, 5), // "logIn"
-QT_MOC_LITERAL(1, 6, 5), // "check"
-QT_MOC_LITERAL(2, 12, 0) // ""
+QT_MOC_LITERAL(0, 5), // "logIn"
+QT_MOC_LITERAL(6, 8), // "swapPage"
+QT_MOC_LITERAL(15, 0), // ""
+QT_MOC_LITERAL(16, 5), // "check"
+QT_MOC_LITERAL(22, 13) // "createProfile"
 
     },
-    "logIn\0check\0"
+    "logIn\0swapPage\0\0check\0createProfile"
 };
 #undef QT_MOC_LITERAL
 
 static const uint qt_meta_data_logIn[] = {
 
  // content:
-       8,       // revision
+      10,       // revision
        0,       // classname
        0,    0, // classinfo
-       1,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
        0,       // signalCount
 
- // slots: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x08 /* Private */,
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   32,    2, 0x08,    1 /* Private */,
+       3,    0,   33,    2, 0x08,    2 /* Private */,
+       4,    0,   34,    2, 0x08,    3 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -66,21 +70,29 @@ void logIn::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void 
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
         auto *_t = static_cast<logIn *>(_o);
-        Q_UNUSED(_t)
+        (void)_t;
         switch (_id) {
-        case 0: _t->check(); break;
+        case 0: _t->swapPage(); break;
+        case 1: _t->check(); break;
+        case 2: _t->createProfile(); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
+    (void)_a;
 }
 
-QT_INIT_METAOBJECT const QMetaObject logIn::staticMetaObject = { {
-    &QMainWindow::staticMetaObject,
-    qt_meta_stringdata_logIn.data,
+const QMetaObject logIn::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_logIn.offsetsAndSize,
     qt_meta_data_logIn,
     qt_static_metacall,
     nullptr,
+qt_incomplete_metaTypeArray<qt_meta_stringdata_logIn_t
+, QtPrivate::TypeAndForceComplete<logIn, std::true_type>
+, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>, QtPrivate::TypeAndForceComplete<void, std::false_type>
+
+
+>,
     nullptr
 } };
 
@@ -104,13 +116,13 @@ int logIn::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 1)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 1;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 1)
-            *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 1;
+        if (_id < 3)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 3;
     }
     return _id;
 }
--- build/ui_logIn.h
+++ build/ui_logIn.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'logIn.ui'
 **
-** Created by: Qt User Interface Compiler version 5.12.8
+** Created by: Qt User Interface Compiler version 6.2.3
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -16,6 +16,7 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QStackedWidget>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -25,12 +26,25 @@ class Ui_logIn
 {
 public:
     QWidget *centralwidget;
-    QLabel *usernameLabel;
-    QLabel *passwordLabel;
-    QLabel *statusLabel;
-    QLineEdit *usernameInput;
+    QStackedWidget *stackedWidget;
+    QWidget *LogIn;
     QLineEdit *passwordInput;
+    QLabel *passwordLabel;
     QPushButton *logInButton;
+    QLineEdit *usernameInput;
+    QLabel *statusLabel;
+    QLabel *usernameLabel;
+    QPushButton *profilePageButton;
+    QWidget *CreateProfile;
+    QLabel *profileStatusLabel;
+    QLabel *profileUsernameLabel;
+    QLineEdit *profileUsernameInput;
+    QLineEdit *profilePasswordInput;
+    QPushButton *profileCreateButton;
+    QLabel *profilePasswordLabel;
+    QLabel *profileFullnameLabel;
+    QLineEdit *profileFullnameInput;
+    QPushButton *profileBackButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -41,31 +55,74 @@ public:
         logIn->resize(800, 600);
         centralwidget = new QWidget(logIn);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-        usernameLabel = new QLabel(centralwidget);
-        usernameLabel->setObjectName(QString::fromUtf8("usernameLabel"));
-        usernameLabel->setGeometry(QRect(290, 110, 101, 23));
-        usernameLabel->setAlignment(Qt::AlignCenter);
-        passwordLabel = new QLabel(centralwidget);
-        passwordLabel->setObjectName(QString::fromUtf8("passwordLabel"));
-        passwordLabel->setGeometry(QRect(290, 190, 101, 23));
-        passwordLabel->setAlignment(Qt::AlignCenter);
-        statusLabel = new QLabel(centralwidget);
-        statusLabel->setObjectName(QString::fromUtf8("statusLabel"));
-        statusLabel->setGeometry(QRect(200, 320, 281, 23));
-        statusLabel->setAlignment(Qt::AlignCenter);
-        usernameInput = new QLineEdit(centralwidget);
-        usernameInput->setObjectName(QString::fromUtf8("usernameInput"));
-        usernameInput->setGeometry(QRect(280, 140, 121, 31));
-        passwordInput = new QLineEdit(centralwidget);
+        stackedWidget = new QStackedWidget(centralwidget);
+        stackedWidget->setObjectName(QString::fromUtf8("stackedWidget"));
+        stackedWidget->setGeometry(QRect(0, -1, 801, 581));
+        LogIn = new QWidget();
+        LogIn->setObjectName(QString::fromUtf8("LogIn"));
+        passwordInput = new QLineEdit(LogIn);
         passwordInput->setObjectName(QString::fromUtf8("passwordInput"));
-        passwordInput->setGeometry(QRect(280, 230, 121, 31));
-        logInButton = new QPushButton(centralwidget);
+        passwordInput->setGeometry(QRect(320, 240, 121, 31));
+        passwordLabel = new QLabel(LogIn);
+        passwordLabel->setObjectName(QString::fromUtf8("passwordLabel"));
+        passwordLabel->setGeometry(QRect(330, 200, 101, 23));
+        passwordLabel->setAlignment(Qt::AlignCenter);
+        logInButton = new QPushButton(LogIn);
         logInButton->setObjectName(QString::fromUtf8("logInButton"));
-        logInButton->setGeometry(QRect(300, 280, 84, 31));
+        logInButton->setGeometry(QRect(340, 290, 84, 31));
+        usernameInput = new QLineEdit(LogIn);
+        usernameInput->setObjectName(QString::fromUtf8("usernameInput"));
+        usernameInput->setGeometry(QRect(320, 150, 121, 31));
+        statusLabel = new QLabel(LogIn);
+        statusLabel->setObjectName(QString::fromUtf8("statusLabel"));
+        statusLabel->setGeometry(QRect(240, 410, 281, 23));
+        statusLabel->setAlignment(Qt::AlignCenter);
+        usernameLabel = new QLabel(LogIn);
+        usernameLabel->setObjectName(QString::fromUtf8("usernameLabel"));
+        usernameLabel->setGeometry(QRect(330, 110, 101, 23));
+        usernameLabel->setAlignment(Qt::AlignCenter);
+        profilePageButton = new QPushButton(LogIn);
+        profilePageButton->setObjectName(QString::fromUtf8("profilePageButton"));
+        profilePageButton->setGeometry(QRect(340, 350, 84, 31));
+        stackedWidget->addWidget(LogIn);
+        CreateProfile = new QWidget();
+        CreateProfile->setObjectName(QString::fromUtf8("CreateProfile"));
+        profileStatusLabel = new QLabel(CreateProfile);
+        profileStatusLabel->setObjectName(QString::fromUtf8("profileStatusLabel"));
+        profileStatusLabel->setGeometry(QRect(240, 390, 281, 23));
+        profileStatusLabel->setAlignment(Qt::AlignCenter);
+        profileUsernameLabel = new QLabel(CreateProfile);
+        profileUsernameLabel->setObjectName(QString::fromUtf8("profileUsernameLabel"));
+        profileUsernameLabel->setGeometry(QRect(310, 160, 141, 23));
+        profileUsernameLabel->setAlignment(Qt::AlignCenter);
+        profileUsernameInput = new QLineEdit(CreateProfile);
+        profileUsernameInput->setObjectName(QString::fromUtf8("profileUsernameInput"));
+        profileUsernameInput->setGeometry(QRect(320, 200, 121, 31));
+        profilePasswordInput = new QLineEdit(CreateProfile);
+        profilePasswordInput->setObjectName(QString::fromUtf8("profilePasswordInput"));
+        profilePasswordInput->setGeometry(QRect(320, 290, 121, 31));
+        profileCreateButton = new QPushButton(CreateProfile);
+        profileCreateButton->setObjectName(QString::fromUtf8("profileCreateButton"));
+        profileCreateButton->setGeometry(QRect(340, 340, 84, 31));
+        profilePasswordLabel = new QLabel(CreateProfile);
+        profilePasswordLabel->setObjectName(QString::fromUtf8("profilePasswordLabel"));
+        profilePasswordLabel->setGeometry(QRect(310, 250, 141, 23));
+        profilePasswordLabel->setAlignment(Qt::AlignCenter);
+        profileFullnameLabel = new QLabel(CreateProfile);
+        profileFullnameLabel->setObjectName(QString::fromUtf8("profileFullnameLabel"));
+        profileFullnameLabel->setGeometry(QRect(300, 70, 161, 23));
+        profileFullnameLabel->setAlignment(Qt::AlignCenter);
+        profileFullnameInput = new QLineEdit(CreateProfile);
+        profileFullnameInput->setObjectName(QString::fromUtf8("profileFullnameInput"));
+        profileFullnameInput->setGeometry(QRect(320, 110, 121, 31));
+        profileBackButton = new QPushButton(CreateProfile);
+        profileBackButton->setObjectName(QString::fromUtf8("profileBackButton"));
+        profileBackButton->setGeometry(QRect(340, 430, 84, 31));
+        stackedWidget->addWidget(CreateProfile);
         logIn->setCentralWidget(centralwidget);
         menubar = new QMenuBar(logIn);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 800, 28));
+        menubar->setGeometry(QRect(0, 0, 800, 20));
         logIn->setMenuBar(menubar);
         statusbar = new QStatusBar(logIn);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -73,16 +130,26 @@ public:
 
         retranslateUi(logIn);
 
+        stackedWidget->setCurrentIndex(1);
+
+
         QMetaObject::connectSlotsByName(logIn);
     } // setupUi
 
     void retranslateUi(QMainWindow *logIn)
     {
-        logIn->setWindowTitle(QApplication::translate("logIn", "logIn", nullptr));
-        usernameLabel->setText(QApplication::translate("logIn", "Username", nullptr));
-        passwordLabel->setText(QApplication::translate("logIn", "Password", nullptr));
-        statusLabel->setText(QApplication::translate("logIn", "status", nullptr));
-        logInButton->setText(QApplication::translate("logIn", "Log in", nullptr));
+        logIn->setWindowTitle(QCoreApplication::translate("logIn", "logIn", nullptr));
+        passwordLabel->setText(QCoreApplication::translate("logIn", "Password", nullptr));
+        logInButton->setText(QCoreApplication::translate("logIn", "Log in", nullptr));
+        statusLabel->setText(QCoreApplication::translate("logIn", "status", nullptr));
+        usernameLabel->setText(QCoreApplication::translate("logIn", "Username", nullptr));
+        profilePageButton->setText(QCoreApplication::translate("logIn", "Create profile", nullptr));
+        profileStatusLabel->setText(QCoreApplication::translate("logIn", "status", nullptr));
+        profileUsernameLabel->setText(QCoreApplication::translate("logIn", "Enter your username", nullptr));
+        profileCreateButton->setText(QCoreApplication::translate("logIn", "Create Profile", nullptr));
+        profilePasswordLabel->setText(QCoreApplication::translate("logIn", "Enter your password", nullptr));
+        profileFullnameLabel->setText(QCoreApplication::translate("logIn", "Enter your full name", nullptr));
+        profileBackButton->setText(QCoreApplication::translate("logIn", "Back", nullptr));
     } // retranslateUi
 
 };
